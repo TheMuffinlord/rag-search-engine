138230d8452927cea277d6e81d481ed3fe42155e
TheMuffinlord <tcgiant@gmail.com>
2026-02-07 18:40:00 -0500

    ch4.3 embedding models

--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -2,11 +2,27 @@
 
 import argparse
 
+from lib.semantic_search import SemanticSearch
+
+def verify_model():
+    SemanticSearchModel = SemanticSearch()
+    print(f"Model loaded: {SemanticSearchModel.model}")
+    print(f"Max sequence length: {SemanticSearchModel.model.max_seq_length}")
+
+
 def main():
     parser = argparse.ArgumentParser(description="Semantic Search CLI")
+    
+
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    verify_parser = subparsers.add_parser("verify", help="Verifies that the LLM has been loaded.")
+
     args = parser.parse_args()
 
     match args.command:
+        case "verify":
+            verify_model()
         case _:
             parser.print_help()
 
