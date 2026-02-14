25492605d02ed83296b9d4562aa932dffadeb2ff
TheMuffinlord <tcgiant@gmail.com>
2026-02-14 12:28:24 -0500

    ch4.11 document embeddings

--- cli/lib/semantic_search.py
+++ cli/lib/semantic_search.py
@@ -1,4 +1,9 @@
 from sentence_transformers import SentenceTransformer
+from constants import CACHE_DIR, DATA_PATH
+import numpy as np
+import os, json
+
+
 
 def embed_text(text):
     newSearch = SemanticSearch()
@@ -12,13 +17,47 @@ def verify_model():
     print(f"Model loaded: {SemanticSearchModel.model}")
     print(f"Max sequence length: {SemanticSearchModel.model.max_seq_length}")
 
+def verify_embeddings():
+    ssm = SemanticSearch()
+    with open(DATA_PATH, 'r') as f:
+        movieList = json.load(f)
+    documents = movieList['movies']
+    embeddings = ssm.load_or_create_embeddings(documents)
+    print(f"Number of docs:   {len(documents)}")
+    print(f"Embeddings shape: {embeddings.shape[0]} vectors in {embeddings.shape[1]} dimensions")
+
 class SemanticSearch:
     def __init__(self, model_name="all-MiniLM-L6-v2"):
         self.model = SentenceTransformer(model_name)
+        self.embeddings = None
+        self.documents = None
+        self.document_map = {}
+        self.embeddings_path = os.path.join(CACHE_DIR, 'movie_embeddings.npy')
         pass
 
     def generate_embedding(self, text):
         if not text or not text.strip():
             raise ValueError("generate embedding whitespace error")
         return self.model.encode([text])[0]
-        
+
+    def build_embeddings(self, documents):
+        self.documents = documents
+        doclist = []
+        for doc in documents:
+            self.document_map[doc['id']] = doc
+            doclist.append(f"{doc['title']}: {doc['description']}")
+        self.embeddings = self.model.encode(doclist, show_progress_bar=True)
+        with open(self.embeddings_path, 'wb') as f:
+            np.save(f, self.embeddings)
+        return self.embeddings
+    
+    def load_or_create_embeddings(self, documents):
+        self.documents = documents
+        for doc in documents:
+            self.document_map[doc['id']] = doc
+        if os.path.exists(self.embeddings_path):
+            with open(self.embeddings_path, 'rb') as f:
+                self.embeddings = np.load(f)
+            if len(self.embeddings) == len(documents):
+                return self.embeddings
+        return self.build_embeddings(documents)
--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -2,7 +2,7 @@
 
 import argparse
 
-from lib.semantic_search import verify_model, embed_text
+from lib.semantic_search import verify_model, embed_text, verify_embeddings
 
 
 #be wary when running the boot.dev cli on these; results may not show success. Run tests by hand to verify.
@@ -18,6 +18,8 @@ def main():
     embed_text_parser = subparsers.add_parser("embed_text", help="Embeds text or something idk")
     embed_text_parser.add_argument("text", type=str, help="Text to be embedded.")
 
+    verify_embeddings_parser = subparsers.add_parser("verify_embeddings", help="Verifies the cached list of embeddings.")
+
     args = parser.parse_args()
 
     match args.command:
@@ -25,6 +27,8 @@ def main():
             verify_model()
         case "embed_text":
             embed_text(args.text)
+        case "verify_embeddings":
+            verify_embeddings()
         case _:
             parser.print_help()
 
