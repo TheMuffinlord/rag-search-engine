9d41ab5044f6d75b5e82dbcbe1174a6c4438443d
TheMuffinlord <tcgiant@gmail.com>
2026-04-01 17:18:37 -0400

    ch12.4 image search. course end

--- cli/lib/multimodal_search.py
+++ cli/lib/multimodal_search.py
@@ -1,16 +1,50 @@
 from PIL import Image
 from sentence_transformers import SentenceTransformer
+from .semantic_search import cosine_similarity
+from .word_actions import load_movies
 
 def verify_image_embedding(image_path):
     mms = MultiModalSearch()
     embedding = mms.embed_image(image_path)
     print(f"Embedding shape: {embedding.shape[0]} dimensions")
 
+def image_search_cmd(image_path):
+    documents = load_movies()
+    mms = MultiModalSearch(documents)
+    results = mms.search_with_image(image_path)
+    for i, result in enumerate(results[:5], 1):
+        print(f"{i}. {result['title']} (similarity: {result['score']:.3f})")
+        print(f"   {result['description'][:200]}")
+        print()
+
 class MultiModalSearch:
-    def __init__(self, model_name="clip-ViT-B-32"):
+    def __init__(self, doc_list: list, model_name="clip-ViT-B-32",):
         self.model = SentenceTransformer(model_name)
+        self.doc_list = doc_list
+        self.texts = []
+        for doc in doc_list:
+            self.texts.append(f"{doc['title']}: {doc['description']}")
+        print("Generating text embeddings. This may take some time.")
+        self.text_embeddings = self.model.encode(self.texts, show_progress_bar=True)
+        print("Embeddings are generated. Moving on.")
 
     def embed_image(self, image_path):
         image_data = Image.open(image_path)
         image_embedding = self.model.encode([image_data])
         return image_embedding[0]
+    
+    def search_with_image(self, image_path):
+        image_embedding = self.embed_image(image_path)
+        embedding_results = []
+        for i, text_embed in enumerate(self.text_embeddings):
+            cosine_sim = cosine_similarity(image_embedding, text_embed)
+            doc = self.doc_list[i]
+            embedding_results.append({
+                "id": doc['id'],
+                "title": doc['title'],
+                "description": doc['description'],
+                "score": cosine_sim
+            })
+        sorted_results = sorted(embedding_results, key=lambda x: x['score'], reverse=True)
+        return sorted_results[:5] #this is the last lesson, i guess we've earned a few hard-coded variables as a treat
+    
--- cli/multimodal_search_cli.py
+++ cli/multimodal_search_cli.py
@@ -1,6 +1,6 @@
 import argparse
 
-from lib.multimodal_search import verify_image_embedding
+from lib.multimodal_search import verify_image_embedding, image_search_cmd
 
 def main():
     parser = argparse.ArgumentParser(description="Multimodal Search parser")
@@ -9,12 +9,18 @@ def main():
     VIE_parser = subparsers.add_parser("verify_image_embedding", help="verifies an image embedding.")
     VIE_parser.add_argument("image_path", type=str, help="Path to image file to verify.")
 
+    IS_parser = subparsers.add_parser("image_search", help="searches movie database for a result based on a given image.")
+    IS_parser.add_argument("image_path", type=str, help="path for the image.")
+
     args = parser.parse_args()
 
     match args.command:
         case "verify_image_embedding":
             image_path = args.image_path
             verify_image_embedding(image_path)
+        case "image_search":
+            image_path = args.image_path
+            image_search_cmd(image_path)
         case _:
             parser.print_help()
 
